5bf472988b4a8973eb7821678b7bc1281c165625
Elwen <coussotelwen@gmail.com>
2025-02-14 21:39:02 +0100

    add link to user in follow list

--- z/src/app/components/followListModal.js
+++ z/src/app/components/followListModal.js
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import Loading from "../loading";
+import Link from "next/link";
+import { LucideLink2 } from "lucide-react";
 
 const FollowListModal = ({ userId, type, onClose }) => {
     const [users, setUsers] = useState([]);
@@ -11,33 +13,33 @@ const FollowListModal = ({ userId, type, onClose }) => {
         const fetchUsers = async () => {
             setLoading(true);
             try {
-            // First fetch to get IDs
-            const res = await fetch(`/api/user/${userId}/${type}`);
-            const data = await res.json();
+                // First fetch to get IDs
+                const res = await fetch(`/api/user/${userId}/${type}`);
+                const data = await res.json();
 
-            if (res.ok) {
-                let userIds = [];
-                if (type === "followers" && data.followers) {
-                userIds = data.followers;
-                } else if (type === "following" && data.following) {
-                userIds = data.following;
+                if (res.ok) {
+                    let userIds = [];
+                    if (type === "followers" && data.followers) {
+                        userIds = data.followers;
+                    } else if (type === "following" && data.following) {
+                        userIds = data.following;
+                    }
+
+                    // Second fetch to get details for each user
+                    const userDetailsPromises = userIds.map(id =>
+                        fetch(`/api/user/${id}`).then(res => res.json())
+                    );
+                    const userDetails = await Promise.all(userDetailsPromises);
+                    setUsers(userDetails);
+                } else {
+                    console.error("Erreur API :", data);
+                    setUsers([]);
                 }
-
-                // Second fetch to get details for each user
-                const userDetailsPromises = userIds.map(id => 
-                fetch(`/api/user/${id}`).then(res => res.json())
-                );
-                const userDetails = await Promise.all(userDetailsPromises);
-                setUsers(userDetails);
-            } else {
-                console.error("Erreur API :", data);
-                setUsers([]);
-            }
             } catch (error) {
-            console.error("Erreur fetch :", error);
-            setUsers([]);
+                console.error("Erreur fetch :", error);
+                setUsers([]);
             } finally {
-            setLoading(false);
+                setLoading(false);
             }
         };
 
@@ -59,9 +61,11 @@ const FollowListModal = ({ userId, type, onClose }) => {
                 ) : (
                     <ul className="mt-3">
                         {users.map((user) => (
-                            <li key={`user-${user.user._id}`} className="flex items-center gap-3 py-2 border-b border-border-dark">
-                                <img src={user.user.avatar} alt={user.user.name} className="w-10 h-10 rounded-full" />
-                                <span>{user.user.name}</span>
+                            <li key={`user-${user.user._id}`} className="flex items-center justify-between py-2 border-b border-border-dark">
+                                <Link href={`/user/${user.user._id}`} className="flex items-center gap-3 w-full hover:bg-secondary-dark/20 rounded-xl p-2">
+                                    <img src={user.user.avatar} alt={user.user.name} className="w-10 h-10 rounded-full" />
+                                    <span>{user.user.name}</span>
+                                </Link>
                             </li>
                         ))}
                     </ul>
