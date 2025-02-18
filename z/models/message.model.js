8517db122affe208524d9b67d370a185715cc4b8
Elwen <coussotelwen@gmail.com>
2025-02-18 10:06:39 +0100

    modification du modèle de message pour rendre le contenu facultatif et ajout d'un fichier multimédia

--- z/models/message.model.js
+++ z/models/message.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const MessageSchema = new mongoose.Schema({
     conversationId: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation", required: true },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true },
+    content: { type: String },
     mediaFiles: [{ type: String }],
 }, {
     timestamps: true
